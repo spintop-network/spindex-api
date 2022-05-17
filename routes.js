6f90be7dd803c18356f65fd3138f053abf8eb55b
T490s <batudal@gmail.com>
2022-05-17 19:54:22 +0300

    homepage data

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-export const getData = () => {
-  fetch("./data.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((jsondata) => console.log(jsondata));
-};
