79acc0aca9ff116a421f47f50e6d258be63944f4
Enes izgi <enesizgi@pm.me>
2024-03-28 14:21:51 +0300

    fix: circulating supply

--- routes/Supply.js
+++ routes/Supply.js
@@ -21,9 +21,20 @@ const fetchTotalBurned = async () => {
 const fetchCirculatingSupply = async () => {
   try {
     const totalSupply = await contract.totalSupply();
-    const anyswapBalance = await contract.balanceOf("0x171a9377C5013bb06Bca8CfE22B9C007f2C319F1");
-    const spinStakedBalance = await contract.balanceOf("0x3B5095a84a5902E963BF6e302fcdBC38771B0C8c");
-    return parseFloat(ethers.utils.formatEther(totalSupply.sub(anyswapBalance).sub(spinStakedBalance)));
+    const [anyswapBalance, spinStakedBalance] = await Promise.all([
+      await contract.balanceOf("0x171a9377C5013bb06Bca8CfE22B9C007f2C319F1"),
+      await contract.balanceOf("0x3B5095a84a5902E963BF6e302fcdBC38771B0C8c")
+    ]);
+    const [balance1, balance2] = await Promise.all([
+      await contract.balanceOf("0x719361F0f9A775deB653410472dcFD3c9E011c5E"),
+      await contract.balanceOf("0x2020501b0B221710E000707696375631d26821B9")
+    ]);
+    const [balance3, balance4] = await Promise.all([
+      await contract.balanceOf("0x9b76d22A0F96785EBDc778576DB1f9F60d7A1D2B"),
+      await contract.balanceOf("0x159f805bBD076bcA897904F0Ca1d18901D1CE9D0")
+    ]);
+    const balance5 = await contract.balanceOf("0x7019E1139949464EF520143eFe37A69EC0E48f45");
+    return parseFloat(ethers.utils.formatEther(totalSupply.sub(anyswapBalance).sub(spinStakedBalance).sub(balance1).sub(balance2).sub(balance3).sub(balance4).sub(balance5)));
   } catch (e) {
     console.error(e)
     return 0
